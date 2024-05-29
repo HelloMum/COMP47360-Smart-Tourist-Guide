5cf0b5d321530e1318fa44df96eff6d5aa4335af
Jorge Durán <jdg@hotmail.es>
2024-05-29 02:53:50 +0100

    Basic implementation of the encapsulated instances

---
+++ src/frontend/react/src/client.js
@@ -0,0 +1,17 @@
+// client.js
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    }
+
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllStudents = () =>
+    fetch("api/v1/students")
+        .then(checkStatus)
+        .then(response => response.json());
