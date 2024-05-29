76dae29e3225a3a45513d3301ca8597c1bb63aa3
Jorge Durán <jdg@hotmail.es>
2024-05-29 02:53:50 +0100

    Basic implementation of the encapsulated instances

---
+++ src/backend/spring/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Gender;
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping(path = "{id}")
+    public Student getStudentById(@PathVariable("id") Long id) {
+        return studentService.getStudentById(id);
+    }
+}
--- src/frontend/react/src/App.js
+++ src/frontend/react/src/App.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import {useEffect, useState} from "react";
+import {getAllStudents} from "./client";
+import {Breadcrumb, Layout, Table} from 'antd';
+import './App.css';
 
+const {Header, Content, Footer} = Layout;
+
+// get data from backend and display it
 function App() {
+    const [students, setStudents] = useState([]);
+
+    const fetchStudents = () => {
+        getAllStudents()
+            .then(data => {
+                setStudents(data);
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error fetching students:", error);
+            });
+    };
+
+    useEffect(() => {
+        console.log("the component is mounted");
+        fetchStudents();
+    }, []);
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email',
+        },
+    ];
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <h1>React App is Working!</h1>
-            </header>
-        </div>
+        <Layout style={{minHeight: '100vh'}}>
+            <Layout>
+                <Header style={{padding: 0, background: '#fff'}}/>
+                <Content style={{margin: '0 16px'}}>
+                    <Breadcrumb style={{margin: '16px 0'}}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{padding: 24, minHeight: 360, background: '#fff'}}>
+                        <Table dataSource={students} columns={columns} rowKey="id"/>
+                    </div>
+                </Content>
+                <Footer style={{textAlign: 'center'}}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
     );
 }
 
