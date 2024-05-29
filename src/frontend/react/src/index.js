03746af3d6747f91881da16cdaeeee8a3ee1393a
Jorge Durán <jdg@hotmail.es>
2024-05-29 02:53:50 +0100

    Basic implementation of the encapsulated instances

--- src/frontend/react/src/index.js
+++ src/frontend/react/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './App.css';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
