2205bf32f9073a4c82b56f04e38ba4579cc8a9e1
Skyeeee <boyu.wang01@outlook.com>
2024-06-03 13:23:42 +0100

    Current Weather Scraper

--- src/backend/scrappers/src/main/java/com/example/demo/config/WebConfig.java
+++ src/backend/scrappers/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,4 +1,4 @@
-package scrappers.src.main.java.com.example.demo.config;
+package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
