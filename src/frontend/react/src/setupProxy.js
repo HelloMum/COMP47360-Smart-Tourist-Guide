972d1ef2bbfc386a01a3ea6008e30ab82201f129
Jorge Durán <jdg@hotmail.es>
2024-06-05 15:36:41 +0100

    Changing autodeploy internal port forward

--- src/frontend/react/src/setupProxy.js
+++ src/frontend/react/src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
     app.use(
         '/api',
         legacyCreateProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: 'http://backend:8080',
             changeOrigin: true,
         })
     );
     app.use(
         '/api2',
         legacyCreateProxyMiddleware({
-            target: 'http://localhost:4000', // this is an example of a different port but not used in this project
+            target: 'http://scrappers:8081', // this is an example of a different port but not used in this project
             changeOrigin: true,
         })
     );
