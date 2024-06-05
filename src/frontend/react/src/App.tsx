7f62400eb6d8c2d2b256f058d327120fee0a4cfe
lasa1015 <luosha1015@gmail.com>
2024-06-05 21:17:32 +0100

    add map

--- src/frontend/react/src/App.tsx
+++ src/frontend/react/src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { LoadScript } from '@react-google-maps/api';
 import Events from './components/Events';
 import Schedule from './components/Schedule';
 import Map from './components/Map';
 import Header from './components/Header';
 import './App.css';
 
+const googleMapsApiKey = "AIzaSyCY1DTFE2IGNPcc54cRmnnSkLvq8VfpMMo";
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/events" element={<Events />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/map" element={<Map />} />
-        </Routes>
-      </div>
-    </Router>
+    <LoadScript googleMapsApiKey={googleMapsApiKey}>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/events" element={<Events />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
+        </div>
+      </Router>
+    </LoadScript>
   );
 }
 
--- src/frontend/react/src/components/Map.tsx
+++ src/frontend/react/src/components/Map.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { GoogleMap } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '90vh'
+};
+
+const center = {
+  lat: 40.783430,
+  lng: -73.966250
+};
 
 const Map: React.FC = () => {
   return (
     <div>
       <h1>Map</h1>
-      {/* 地图显示和路线规划逻辑 */}
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+      >
+        { /* You can add more components here */ }
+      </GoogleMap>
     </div>
   );
 };
