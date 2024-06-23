aa2235ecc7ca27efc65e4f337f0f6057a440928e
Skyeeee <boyu.wang01@outlook.com>
2024-06-23 22:45:48 +0100

    Improve logic of events scraper. Delete all the data before fetching new data. Check and skip duplicate events

--- src/backend/scrappers/src/main/java/com/example/demo/repository/EventRepository.java
+++ src/backend/scrappers/src/main/java/com/example/demo/repository/EventRepository.java
@@ -2,9 +2,20 @@ package com.example.demo.repository;
 
 import com.example.demo.model.EventData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface EventRepository extends JpaRepository<EventData, UUID> {
-    boolean existsByNameAndLatitudeAndLongitude(String name, double latitude, double longitude);
+
+    @Query("SELECT COUNT(e) > 0 FROM EventData e WHERE " +
+            "e.name = :name AND " +
+            "e.category = :category AND " +
+            "e.description = :description ")
+    boolean existsEvent(@Param("name") String name,
+                        @Param("category") String category,
+                        @Param("description") String description);
 }
