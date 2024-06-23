66588f724b9bb4093b2cd6f05c82cf51661c0566
Skyeee3 <125045068+Skyeee3@users.noreply.github.com>
2024-06-23 23:08:33 +0100

    Filter logic boyu (#52)
    * Improve logic about name of events in api endpoint
    
    * Improve logic of events scraper. Delete all the data before fetching new data. Check and skip duplicate events
    
    ---------
    
    Co-authored-by: Jorge Durán <67933217+HelloMum@users.noreply.github.com>

--- src/backend/scrappers/src/main/java/com/example/demo/service/EventService.java
+++ src/backend/scrappers/src/main/java/com/example/demo/service/EventService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -28,39 +29,36 @@ public class EventService {
     @Value("${yelp.limit}")
     private int limit;
 
-    private LocalDateTime lastFetchTime = LocalDateTime.now();
-    private int offset = 0;
-
     private static final double[][] ManhattanArea = {
-        {-74.4900261866312, 40.68686056618563},
-        {-74.02247557630322, 40.68060952571861},
-        {-74.00673931790989, 40.69061065576909},
-        {-74.00158758353581, 40.70353031583642},
-        {-73.9742063962419, 40.709181659436126},
-        {-73.96306764293499, 40.751602379032676},
-        {-73.9381111465915, 40.77824950423954},
-        {-73.93571826054003, 40.781756164126875},
-        {-73.93835978383461, 40.78482868075099},
-        {-73.93197657063641, 40.791936029479984},
-        {-73.92872339624913, 40.79449340657828},
-        {-73.92810629188814, 40.80216517440272},
-        {-73.93381326053665, 40.80916985685798},
-        {-73.9329464094588, 40.81800623927862},
-        {-73.93445994426258, 40.834713196012274},
-        {-73.92834261566213, 40.84628029333956},
-        {-73.92006444104246, 40.85808855226034},
-        {-73.91165332965423, 40.86651028933875},
-        {-73.9095237173859, 40.87232949008461},
-        {-73.91295888114882, 40.87465683738827},
-        {-73.91793162880116, 40.87546532210828},
-        {-73.92124698194723, 40.8763637510383},
-        {-73.92318565996821, 40.87815766160858},
-        {-73.9291707921706, 40.8787628614993},
-        {-73.95417515817823, 40.85108001891453},
-        {-74.01551813710326, 40.75627158256748},
-        {-74.03478207315514, 40.70272068669027},
-        {-74.04416204237901, 40.70052281665727},
-        {-74.04903271144373, 40.68685333783202}
+            {-74.4900261866312, 40.68686056618563},
+            {-74.02247557630322, 40.68060952571861},
+            {-74.00673931790989, 40.69061065576909},
+            {-74.00158758353581, 40.70353031583642},
+            {-73.9742063962419, 40.709181659436126},
+            {-73.96306764293499, 40.751602379032676},
+            {-73.9381111465915, 40.77824950423954},
+            {-73.93571826054003, 40.781756164126875},
+            {-73.93835978383461, 40.78482868075099},
+            {-73.93197657063641, 40.791936029479984},
+            {-73.92872339624913, 40.79449340657828},
+            {-73.92810629188814, 40.80216517440272},
+            {-73.93381326053665, 40.80916985685798},
+            {-73.9329464094588, 40.81800623927862},
+            {-73.93445994426258, 40.834713196012274},
+            {-73.92834261566213, 40.84628029333956},
+            {-73.92006444104246, 40.85808855226034},
+            {-73.91165332965423, 40.86651028933875},
+            {-73.9095237173859, 40.87232949008461},
+            {-73.91295888114882, 40.87465683738827},
+            {-73.91793162880116, 40.87546532210828},
+            {-73.92124698194723, 40.8763637510383},
+            {-73.92318565996821, 40.87815766160858},
+            {-73.9291707921706, 40.8787628614993},
+            {-73.95417515817823, 40.85108001891453},
+            {-74.01551813710326, 40.75627158256748},
+            {-74.03478207315514, 40.70272068669027},
+            {-74.04416204237901, 40.70052281665727},
+            {-74.04903271144373, 40.68685333783202}
     };
 
     // Determine whether a point is inside Manhattan
@@ -80,12 +78,17 @@ public class EventService {
     }
 
     @Scheduled(fixedRate = 10800000)
+    @Transactional
     public void fetchAndSaveEvents() {
-        long startDate = lastFetchTime.toEpochSecond(ZoneOffset.UTC);
-        List<EventData> events = eventScraper.fetchYelpEvents(offset, startDate);
+        // Delete all the data
+        eventRepository.deleteAll();
+
+        // Fetch the new data
+        long startDate = LocalDateTime.now().minusDays(1).toEpochSecond(ZoneOffset.UTC);
+        List<EventData> events = eventScraper.fetchYelpEvents(0, startDate);
+
         if (events != null && !events.isEmpty()) {
             List<EventData> newEvents = events.stream()
-                    .filter(event -> !eventRepository.existsByNameAndLatitudeAndLongitude(event.getName(), event.getLatitude(), event.getLongitude()))
                     .filter(event -> !event.getIs_canceled())
                     .filter(event -> {
                         LocalDateTime timeStart = convertToLocalDateTime(event.getTime_start());
@@ -97,26 +100,28 @@ public class EventService {
                     .filter(event -> isPointInPolygon(event.getLatitude(), event.getLongitude(), ManhattanArea))
                     .collect(Collectors.toList());
 
-            newEvents.forEach(event -> {
-                event.setId(UUID.randomUUID());
-                event.setFetchTime(LocalDateTime.now());
-                if (event.getTime_end() == null) {
-                    LocalDateTime timeStart = convertToLocalDateTime(event.getTime_start());
-                    event.setTime_end(formatLocalDateTime(timeStart.plusHours(2)));
-                } else {
-                    event.setTime_end(formatLocalDateTime(convertToLocalDateTime(event.getTime_end())));
-                }
-                event.setTime_start(formatLocalDateTime(convertToLocalDateTime(event.getTime_start())));
-            });
-            eventRepository.saveAll(newEvents);
+            // Skip the duplicate events
+            for (EventData event : newEvents) {
+                boolean exists = eventRepository.existsEvent(
+                        event.getName(),
+                        event.getCategory(),
+                        event.getDescription()
+                );
 
-            offset += events.size();
-            if (events.size() < limit) {
-                offset = 0;
-                lastFetchTime = LocalDateTime.now();
+                if (!exists) {
+                    event.setId(UUID.randomUUID());
+                    event.setFetchTime(LocalDateTime.now());
+                    if (event.getTime_end() == null) {
+                        LocalDateTime timeStart = convertToLocalDateTime(event.getTime_start());
+                        event.setTime_end(formatLocalDateTime(timeStart.plusHours(2)));
+                    } else {
+                        event.setTime_end(formatLocalDateTime(convertToLocalDateTime(event.getTime_end())));
+                    }
+                    event.setTime_start(formatLocalDateTime(convertToLocalDateTime(event.getTime_start())));
+                    eventRepository.save(event);
+                }
             }
         }
-        deleteUselessEvents();
     }
 
     private LocalDateTime convertToLocalDateTime(String dateTimeWithOffset) {
@@ -128,39 +133,7 @@ public class EventService {
         return localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
     }
 
-
-
-    public void deleteUselessEvents() {
-        List<EventData> allEvents = eventRepository.findAll();
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-
-        List<EventData> uselessEvents = allEvents.stream()
-                .filter(event -> event.getIs_canceled())
-                .filter(event -> {
-                    try {
-                        LocalDateTime timeStart = LocalDateTime.parse(event.getTime_start(), formatter);
-                        return timeStart.isAfter(now.plusDays(30));
-                    } catch (Exception e) {
-                        return false;
-                    }
-                })
-                .filter(event -> {
-                    try {
-                        LocalDateTime timeEnd = LocalDateTime.parse(event.getTime_end(), formatter);
-                        return timeEnd.isBefore(now);
-                    } catch (Exception e) {
-                        return true;
-                    }
-                })
-                .filter(event -> !isPointInPolygon(event.getLatitude(), event.getLongitude(), ManhattanArea))
-                .collect(Collectors.toList());
-
-        eventRepository.deleteAll(uselessEvents);
-    }
-
     public List<EventData> getAllEvents() {
         return eventRepository.findAll();
     }
 }
-
