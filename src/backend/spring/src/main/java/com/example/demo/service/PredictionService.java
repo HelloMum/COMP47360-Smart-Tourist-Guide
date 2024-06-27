2e3eb141cf2ad3774437ffd6eef9e2ea265928db
Jorge Durán <67933217+HelloMum@users.noreply.github.com>
2024-06-27 10:58:50 +0100

    Bugfix: Change how the resources are loaded (#63)

--- src/backend/spring/src/main/java/com/example/demo/service/PredictionService.java
+++ src/backend/spring/src/main/java/com/example/demo/service/PredictionService.java
@@ -1,26 +1,20 @@
 package com.example.demo.service;
 
-import lombok.Getter;
 import ml.dmlc.xgboost4j.java.Booster;
 import ml.dmlc.xgboost4j.java.DMatrix;
 import ml.dmlc.xgboost4j.java.XGBoost;
 import ml.dmlc.xgboost4j.java.XGBoostError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * Service class for making predictions using a pre-trained XGBoost model.
- * <p>
- * This service loads the XGBoost model from the specified file in the classpath
- * and provides a method to make predictions based on input features.
  */
-@Getter
 @Service
 public class PredictionService {
 
@@ -29,28 +23,23 @@ public class PredictionService {
 
     /**
      * Constructs a PredictionService and loads the XGBoost model.
-     * <p>
-     * The model file is expected to be located in the classpath at "mlm/XGboost_model_depth_12_lr_0.1_estimators_200_2.bin".
-     * If the model file cannot be loaded, an error is logged.
      */
     public PredictionService() {
         try {
             // Load the XGBoost model
-            File file = ResourceUtils.getFile("classpath:mlm/XGboost_model_depth_12_lr_0.1_estimators_200_2.bin");
-            InputStream modelStream = new FileInputStream(file);
-            logger.info("Loading XGBoost model from: " + file.getPath());
+            ClassPathResource resource = new ClassPathResource("mlm/XGboost_model_depth_12_lr_0.1_estimators_200_2.bin");
+            InputStream modelStream = resource.getInputStream();
+            logger.info("Loading XGBoost model from: " + resource.getURL().getPath());
             booster = XGBoost.loadModel(modelStream);
             logger.info("XGBoost model loaded successfully.");
-        } catch (XGBoostError e) {
+        } catch (IOException | XGBoostError e) {
             logger.error("Failed to load XGBoost model.", e);
-        } catch (Exception e) {
-            logger.error("Unexpected error during model loading.", e);
         }
     }
 
     /**
      * Makes a prediction based on the input features.
-     * <p>
+     *
      * @param features an array of double values representing the input features.
      * @return a float array containing the prediction results.
      * @throws XGBoostError if an error occurs during prediction.
@@ -63,10 +52,19 @@ public class PredictionService {
         }
 
         // Create DMatrix from the float array
-        DMatrix dmatrix = new DMatrix(floatFeatures, 1, features.length, Float.NaN);
+        DMatrix dmatrix;
+        try {
+            dmatrix = new DMatrix(floatFeatures, 1, features.length, Float.NaN);
+        } catch (XGBoostError e) {
+            throw new RuntimeException("Failed to create DMatrix for prediction.", e);
+        }
 
         // Predict
-        float[][] predictions = booster.predict(dmatrix);
-        return predictions[0];
+        try {
+            float[][] predictions = booster.predict(dmatrix);
+            return predictions[0];
+        } catch (XGBoostError e) {
+            throw new XGBoostError("Failed to make predictions with XGBoost model.", e);
+        }
     }
 }
