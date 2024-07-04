bcad704004f5f6bdd648f9c09e03f34268337388
lasa1015 <luosha1015@gmail.com>
2024-07-04 20:17:24 +0100

    change alert message

--- src/frontend/react/src/components/events/EventCard.tsx
+++ src/frontend/react/src/components/events/EventCard.tsx
@@ -5,6 +5,7 @@ import Btn_Add from '../Btn_Add';
 import Tag_Category from '../Tag_Category';
 import Tag_IsFree from '../Tag_IsFree';
 import { ListContext } from '../../contexts/ListContext';
+import AlertModal from '../AlertModal';
 
 interface Event {
   id: string | number;
@@ -36,11 +37,12 @@ const EventCard: React.FC<EventCardProps> = ({ event, onMouseEnter, onMouseLeave
   const [isExpanded, setIsExpanded] = useState(false);
   const { date, time } = formatDateTime(event.time_start);
   const imageUrl = event.image_url || "images/events/default.jpg";
-  const { listItems, addItemWithDateCheck, isItemInList } = useContext(ListContext);
+  const { addItemWithDateCheck, isItemInList } = useContext(ListContext);
+  const [alertOpen, setAlertOpen] = useState(false);
 
   const handleAdd = () => {
     const eventData = { id: event.id, title: event.name, image: imageUrl };
-    addItemWithDateCheck(eventData, () => alert('Please set the start and end dates before adding items to the list.'));
+    addItemWithDateCheck(eventData, () => setAlertOpen(true));
   };
 
   const toggleExpand = () => setIsExpanded(!isExpanded);
@@ -48,96 +50,105 @@ const EventCard: React.FC<EventCardProps> = ({ event, onMouseEnter, onMouseLeave
   const isAdded = isItemInList(event.id);
 
   return (
-    <Card
-      sx={{
-        borderRadius: '7px',
-        overflow: 'hidden',
-        boxShadow: '0 1px 5px rgba(0, 0, 0, 0.15)',
-        width: "98%",
-        paddingX: '20px',
-        paddingY: '12px',
-        marginBottom: 3,
-        marginLeft: '3px',
-        marginTop: '3px',
-        height: isExpanded ? 'auto' : '170px',
-        transition: 'box-shadow 0.3s',
-        '&:hover': {
-          boxShadow: '0 3px 10px rgba(0, 0, 0, 0.3)' 
-        }
-      }}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
-      <Stack direction="row">
-        <Box sx={{ position: 'relative', width: '180px', height: isExpanded ? '190px' : '128px', boxShadow: 1, borderRadius: '4px', marginTop: '10px' }}>
-          <CardMedia
-            component="img"
-            height="100"
-            image={imageUrl}
-            alt={event.name}
-            sx={{ objectFit: 'cover', borderRadius: '4px', width: '180px', height: isExpanded ? '190px' : '128px' }}
-          />
-        </Box>
+    <>
+      <Card
+        sx={{
+          borderRadius: '7px',
+          overflow: 'hidden',
+          boxShadow: '0 1px 5px rgba(0, 0, 0, 0.15)',
+          width: "98%",
+          paddingX: '20px',
+          paddingY: '12px',
+          marginBottom: 3,
+          marginLeft: '3px',
+          marginTop: '3px',
+          height: isExpanded ? 'auto' : '170px',
+          transition: 'box-shadow 0.3s',
+          '&:hover': {
+            boxShadow: '0 3px 10px rgba(0, 0, 0, 0.3)' 
+          }
+        }}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        <Stack direction="row">
+          <Box sx={{ position: 'relative', width: '180px', height: isExpanded ? '190px' : '128px', boxShadow: 1, borderRadius: '4px', marginTop: '10px' }}>
+            <CardMedia
+              component="img"
+              height="100"
+              image={imageUrl}
+              alt={event.name}
+              sx={{ objectFit: 'cover', borderRadius: '4px', width: '180px', height: isExpanded ? '190px' : '128px' }}
+            />
+          </Box>
 
-        <Box sx={{ marginLeft: '20px', flexGrow: 1 }}>
-          <Typography
-            sx={{
-              whiteSpace: isExpanded ? 'normal' : 'nowrap',
-              fontSize: '1.05rem',
-              fontWeight: 400,
-              overflow: 'hidden',
-              textOverflow: isExpanded ? 'clip' : 'ellipsis',
-              maxWidth: '22vw',
-              display: 'inline-block',
-            }}
-            component="div"
-            title={event.name}
-          >
-            {event.name}
-          </Typography>
+          <Box sx={{ marginLeft: '20px', flexGrow: 1 }}>
+            <Typography
+              sx={{
+                whiteSpace: isExpanded ? 'normal' : 'nowrap',
+                fontSize: '1.05rem',
+                fontWeight: 400,
+                overflow: 'hidden',
+                textOverflow: isExpanded ? 'clip' : 'ellipsis',
+                maxWidth: '22vw',
+                display: 'inline-block',
+              }}
+              component="div"
+              title={event.name}
+            >
+              {event.name}
+            </Typography>
 
-          <Stack direction='row' spacing={1}>
-            <Tag_Category category={event.combined_category} />
-            {event.is_free && <Tag_IsFree isFree={event.is_free} />}
-            <PublicRounded sx={{ fontSize: 'large', marginRight: 1, cursor: 'pointer' }} onClick={() => window.open(event.event_site_url, '_blank')} />
-          </Stack>
+            <Stack direction='row' spacing={1}>
+              <Tag_Category category={event.combined_category} />
+              {event.is_free && <Tag_IsFree isFree={event.is_free} />}
+              <PublicRounded sx={{ fontSize: 'large', marginRight: 1, cursor: 'pointer' }} onClick={() => window.open(event.event_site_url, '_blank')} />
+            </Stack>
 
-          <Stack gap={'4px'} marginTop={'2px'}>
-            <Box display="flex" alignItems="center" marginTop={1}>
-              <DateRangeRounded sx={{ fontSize: 'large', marginRight: 1 }} />
-              <Typography variant="body2" color="text.secondary" style={{ marginRight: '50px' }}>
-                {date}
-              </Typography>
-              <AccessTimeRounded sx={{ fontSize: 'large', marginRight: 1 }} />
-              <Typography variant="body2" color="text.secondary">
-                {time}
-              </Typography>
-            </Box>
-
-            {event.address && (
-              <Box display="flex" alignItems="center">
-                <LocationOnRounded sx={{ fontSize: 'large', marginRight: 1 }} />
+            <Stack gap={'4px'} marginTop={'2px'}>
+              <Box display="flex" alignItems="center" marginTop={1}>
+                <DateRangeRounded sx={{ fontSize: 'large', marginRight: 1 }} />
+                <Typography variant="body2" color="text.secondary" style={{ marginRight: '50px' }}>
+                  {date}
+                </Typography>
+                <AccessTimeRounded sx={{ fontSize: 'large', marginRight: 1 }} />
                 <Typography variant="body2" color="text.secondary">
-                  {event.address}
+                  {time}
                 </Typography>
               </Box>
-            )}
 
-            {isExpanded && (
-              <Typography variant="body2" color="text.secondary" sx={{ marginLeft: '3px' }}>
-                {event.description}
-              </Typography>
-            )}
-          </Stack>
+              {event.address && (
+                <Box display="flex" alignItems="center">
+                  <LocationOnRounded sx={{ fontSize: 'large', marginRight: 1 }} />
+                  <Typography variant="body2" color="text.secondary">
+                    {event.address}
+                  </Typography>
+                </Box>
+              )}
 
-          <Stack direction='row' justifyContent="space-between" sx={{ width: '95%', paddingY: 1 }}>
-            <Btn_Add onClick={handleAdd} isAdded={isAdded} />
-            {!isExpanded && <IconButton onClick={toggleExpand}><ExpandMoreRounded /></IconButton>}
-            {isExpanded && <IconButton onClick={toggleExpand}><ExpandLessRounded /></IconButton>}
-          </Stack>
-        </Box>
-      </Stack>
-    </Card>
+              {isExpanded && (
+                <Typography variant="body2" color="text.secondary" sx={{ marginLeft: '3px' }}>
+                  {event.description}
+                </Typography>
+              )}
+            </Stack>
+
+            <Stack direction='row' justifyContent="space-between" sx={{ width: '95%', paddingY: 1 }}>
+              <Btn_Add onClick={handleAdd} isAdded={isAdded} />
+              {!isExpanded && <IconButton onClick={toggleExpand}><ExpandMoreRounded /></IconButton>}
+              {isExpanded && <IconButton onClick={toggleExpand}><ExpandLessRounded /></IconButton>}
+            </Stack>
+          </Box>
+        </Stack>
+      </Card>
+
+      <AlertModal
+        open={alertOpen}
+        onClose={() => setAlertOpen(false)}
+        title="Warning"
+        message="Please set the start and end dates before adding items to the list."
+      />
+    </>
   );
 };
 
