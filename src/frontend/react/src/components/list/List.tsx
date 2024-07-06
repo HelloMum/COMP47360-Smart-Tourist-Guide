10a60de8dabe4dc7a0119a96d5279f0fb496786d
Sha Luo <40826547+lasa1015@users.noreply.github.com>
2024-07-06 15:29:12 +0100

    make date range picker work, restyle of home page (#80)
    * make date range picker work
    
    * make date range picker work
    
    * only can add tourist attratction when date is picked
    
    * primitive schedule data display
    
    * primitive schedule data display
    
    * css details
    
    * css details
    
    * continue basic list and schedule
    
    * change alert message
    
    * details
    
    * css details
    
    * restyle home page
    
    * home page
    
    * update of landing page
    
    * landing page details
    
    * css details

--- src/frontend/react/src/components/list/List.tsx
+++ src/frontend/react/src/components/list/List.tsx
@@ -1,16 +1,22 @@
 import React, { useContext, useState } from 'react';
-import { Box, Button, Typography, Snackbar, Alert } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { ListContext } from '../../contexts/ListContext';
 import ListCard from './ListCard';
 import { NAVBAR_HEIGHT } from '../../constants';
 import Btn_Close_List from './Btn_Close_List';
+import moment from 'moment';
+import { ClearRounded } from '@mui/icons-material';
+import AlertModal from '../AlertModal';
 
 interface ListProps {
   onClose: () => void;
 }
 
 const List: React.FC<ListProps> = ({ onClose }) => {
-  const { listItems, removeFromList } = useContext(ListContext);
+  const { listItems, removeFromList, selectedDates, setPlanData, clearList } = useContext(ListContext);
+  const navigate = useNavigate();
+  const [alertOpen, setAlertOpen] = useState(false);
 
   // Validate the selection format
   const validateSelection = (selection) => {
@@ -24,26 +30,30 @@ const List: React.FC<ListProps> = ({ onClose }) => {
 
   // click 'generate' btn to post data to backend
   const handleGeneratePlan = async () => {
+    if (listItems.length === 0) {
+      setAlertOpen(true);
+      return;
+    }
+
     const selection = {
-      ids: [
-        "141",
-        "22",
-        "157",
-        "243eeb69-7b45-41cc-b65d-ae422f70aa23",
-         "4c57d62e-a8d3-432b-a8e6-d63c5a2bce9c"
-      ]
+      ids: listItems.map(item => item.id)
     };
-  
-    const startDate = '2024-07-05';  // Example start date
-    const endDate = '2024-07-07';    // Example end date
-  
+
+    if (!selectedDates || !selectedDates[0] || !selectedDates[1]) {
+      console.error('Error: Start date and end date must be selected.');
+      return;
+    }
+
+    const startDate = selectedDates[0].format('YYYY-MM-DD');
+    const endDate = selectedDates[1].format('YYYY-MM-DD');
+
     try {
       // Validate data format
       validateSelection(selection);
-  
+
       // Log the data to be sent
-      console.log('Data to be sent to backend:',  selection );
-  
+      console.log('Data to be sent to backend:', selection, startDate, endDate);
+
       const response = await fetch(`http://localhost:8080/itinerary/create?startDate=${startDate}&endDate=${endDate}`, {
         method: 'POST',
         headers: {
@@ -51,24 +61,21 @@ const List: React.FC<ListProps> = ({ onClose }) => {
         },
         body: JSON.stringify(selection),
       });
-  
+
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(errorText);
       }
-  
+
       const data = await response.json();
+      // Log the data received from the backend
       console.log('Data received from backend:', data);
-     
-   
+      setPlanData(data);
+      navigate('/schedule');
     } catch (error) {
       console.error('Error sending data to backend:', error);
-   
-    } finally {
-
     }
   };
-  
 
   return (
     <>
@@ -85,7 +92,7 @@ const List: React.FC<ListProps> = ({ onClose }) => {
         }}
       >
         <Box sx={{ padding: '16px', position: 'sticky', top: 0, backgroundColor: 'white', zIndex: 1001 }}>
-          <Box 
+          <Box
             sx={{
               display: 'flex',
               justifyContent: 'center',
@@ -98,36 +105,79 @@ const List: React.FC<ListProps> = ({ onClose }) => {
               sx={{
                 borderRadius: '100px',
                 width: '150px',
-                height: '35px',
+                height: '32px',
                 fontSize: '14px',
                 boxShadow: 0,
                 borderColor: '#4CAF50',
                 color: '#4CAF50',
-                borderWidth:'1.5px',
+                borderWidth: '1.5px',
                 '&:hover': {
-                  borderColor: '#388E3C', 
-                  backgroundColor: 'rgba(76, 175, 80, 0.3)', 
+                  borderColor: '#388E3C',
+                  backgroundColor: 'rgba(76, 175, 80, 0.3)',
                 },
                 '&:active': {
-                  borderColor: '#388E3C', 
-                  backgroundColor: 'rgba(76, 175, 80, 0.12)', 
+                  borderColor: '#388E3C',
+                  backgroundColor: 'rgba(76, 175, 80, 0.12)',
                   color: '#388E3C',
                 },
               }}
               onClick={handleGeneratePlan}
             >
               Generate Plan
-            </Button>
+            </Button> 
           </Box>
+          
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginX: '10px',
+            }}
+          >
+            <Typography variant="h6">{listItems.length} items</Typography>
 
-          <Typography variant="h6">{listItems.length} items</Typography>
+
+
+{  listItems.length >0   &&      (<Button
+              variant="outlined"
+              startIcon={<ClearRounded sx={{ fontSize: '16px', marginRight: '-6px' }} />} 
+              sx={{
+                borderRadius: '4px',
+                paddingX: '4px',
+                paddingY: '1px',
+                fontSize: '10px',
+                boxShadow: 0,
+                borderColor: 'orange',
+                color: 'orange',
+                borderWidth: '1px',
+                '&:hover': {
+                  borderColor: 'orange',
+                  backgroundColor: 'rgba(250, 161, 54, 0.3)',
+                },
+                '&:active': {
+                  borderColor: 'orange',
+                  backgroundColor: 'rgba(250, 161, 54, 0.12)',
+                  color: 'orange',
+                },
+              }}
+              onClick={clearList}
+            >
+              Clear All
+            </Button>  )}
+
+
+
+
+          </Box>
         </Box>
 
         <Box
           sx={{
             padding: '16px',
+            paddingTop: '0px',
             overflowY: 'auto',
-            height: 'calc(100% - 64px)',
+            height: 'calc(100% - 120px)',
             '&::-webkit-scrollbar': {
               width: '8px',
             },
@@ -154,11 +204,16 @@ const List: React.FC<ListProps> = ({ onClose }) => {
             />
           ))}
         </Box>
-
-   
       </Box>
 
       <Btn_Close_List onClose={onClose} />
+
+      <AlertModal
+        open={alertOpen}
+        onClose={() => setAlertOpen(false)}
+        title="Warning"
+        message="Please add travel spots or events to your list first."
+      />
     </>
   );
 };
--- src/frontend/react/src/components/spots/SpotCard.tsx
+++ src/frontend/react/src/components/spots/SpotCard.tsx
@@ -6,11 +6,12 @@ import Btn_Like from '../Btn_Like';
 import Tag_Category from '../Tag_Category';
 import Tag_IsFree from '../Tag_IsFree';
 import { ListContext } from '../../contexts/ListContext';
+import AlertModal from '../AlertModal';
 
 interface SpotCardProps {
   id: string;
-  image1: string;  
-  image3: string; 
+  image1: string;
+  image3: string;
   title: string;
   address: string;
   rating: number;
@@ -23,73 +24,84 @@ interface SpotCardProps {
 const SpotCard: React.FC<SpotCardProps> = ({ id, image1, image3, title, rating, category, user_ratings_total, onExpand }) => {
   const [currentImage, setCurrentImage] = useState(image1);
   const [imageStyle, setImageStyle] = useState({});
-  const { addToList } = useContext(ListContext);
+  const { addItemWithDateCheck, isItemInList } = useContext(ListContext);
+  const [alertOpen, setAlertOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setCurrentImage(image3);
     setImageStyle({
-      transition: 'transform 7s ease',  
-      transform: 'scale(1.4)' 
+      transition: 'transform 7s ease',
+      transform: 'scale(1.4)',
     });
   };
 
   const handleMouseLeave = () => {
     setCurrentImage(image1);
     setImageStyle({
-      transition: 'none',  
-      transform: 'scale(1)'  
+      transition: 'none',
+      transform: 'scale(1)',
     });
   };
 
   const handleAdd = () => {
     const spotData = { id, title, image: image1 };
-    addToList(spotData);
-    console.log('Add:', spotData);
+    addItemWithDateCheck(spotData, () => setAlertOpen(true));
   };
 
+  const isAdded = isItemInList(id);
+
   return (
-    <Card sx={{ borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.15)', width: "17.5vw", marginBottom: 2, gap:1 }}>
-      <Box sx={{ position: 'relative', overflow: 'hidden' }}>
-        <CardMedia 
-          component="img" 
-          height="170" 
-          image={currentImage} 
-          alt={title} 
-          sx={{ borderRadius: '0px', boxShadow: 0, ...imageStyle }} 
-          onMouseEnter={handleMouseEnter} 
-          onMouseLeave={handleMouseLeave} 
-        />
-        <Btn_Like/>
-      </Box>
-      <Stack sx={{ paddingTop: '8px', paddingLeft: '12px' }}>
-      <Typography
-          sx={{ 
-            fontSize: '1.1rem',
-            fontWeight: 400,
-            overflow: 'hidden',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-            maxWidth: '95%' 
-          }}
-          component="div"
-        >
-          {title}
-        </Typography>
-     
-        <Stack direction={"row"} gap={1}>
-          <Rating name="half-rating-read" defaultValue={rating} precision={0.1} readOnly  sx={{ fontSize: '1.2rem' }}  /> 
-          {rating}<span style={{ color: '#888' }}>by {user_ratings_total} people</span>
+    <>
+      <Card sx={{ borderRadius: '8px', overflow: 'hidden', boxShadow: '0 2px 2px rgba(0, 0, 0, 0.15)', width: "17.5vw", marginBottom: 2, gap: 1 }}>
+        <Box sx={{ position: 'relative', overflow: 'hidden' }}>
+          <CardMedia
+            component="img"
+            height="170"
+            image={currentImage}
+            alt={title}
+            sx={{ borderRadius: '0px', boxShadow: 0, ...imageStyle }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+          <Btn_Like />
+        </Box>
+        <Stack sx={{ paddingTop: '8px', paddingLeft: '12px' }}>
+          <Typography
+            sx={{
+              fontSize: '1.1rem',
+              fontWeight: 400,
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              maxWidth: '95%',
+            }}
+            component="div"
+          >
+            {title}
+          </Typography>
+
+          <Stack direction={"row"} gap={1}>
+            <Rating name="half-rating-read" defaultValue={rating} precision={0.1} readOnly sx={{ fontSize: '1.2rem' }} />
+            {rating}<span style={{ color: '#888' }}>by {user_ratings_total} people</span>
+          </Stack>
+          <Stack direction="row">
+            <Tag_Category category={category} />
+            <Tag_IsFree isFree={true} />
+          </Stack>
+          <Stack direction='row' justifyContent="space-between" sx={{ width: '95%', paddingTop: 1.5 }}>
+            <Btn_Add onClick={handleAdd} isAdded={isAdded} />
+            <IconButton onClick={onExpand}><ExpandMoreRounded /></IconButton>
+          </Stack>
         </Stack>
-        <Stack direction="row">
-          <Tag_Category category={category} />
-          <Tag_IsFree isFree={true} />
-        </Stack>
-        <Stack direction='row' justifyContent="space-between" sx={{ width: '95%', paddingTop: 1.5 }}>
-          <Btn_Add onClick={handleAdd} />
-          <IconButton onClick={onExpand}><ExpandMoreRounded /></IconButton>
-        </Stack>
-      </Stack>
-    </Card>
+      </Card>
+
+      <AlertModal
+        open={alertOpen}
+        onClose={() => setAlertOpen(false)}
+        title="Warning"
+        message="Please set the start and end dates before adding items to the list."
+      />
+    </>
   );
 };
 
