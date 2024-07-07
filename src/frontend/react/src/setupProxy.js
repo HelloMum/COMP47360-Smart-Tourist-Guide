bcf6eac81af8980d866c604a3ec17268277f08ff
Jorge Duran <jdg@hotmail.es>
2024-07-07 19:17:42 +0100

    Proxy setup for localhost

--- src/frontend/react/src/setupProxy.js
+++ src/frontend/react/src/setupProxy.js
@@ -1,10 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    const backendUrl = process.env.BACKEND_URL || 'http://localhost:8080';
+    console.log('Proxying to backend URL:', backendUrl);
+
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: backendUrl,
             changeOrigin: true,
             pathRewrite: {
                 '^/api': '', // Remove the /api prefix when forwarding requests
