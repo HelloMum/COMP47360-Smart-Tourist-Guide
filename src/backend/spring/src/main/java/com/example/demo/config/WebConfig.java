fa789d19b68e169ff6ad6c20883075363a919bb6
Jorge Durán <67933217+HelloMum@users.noreply.github.com>
2024-07-08 14:29:35 +0100

    Setup new proxy system CORS on production (#83)

--- src/backend/spring/src/main/java/com/example/demo/config/WebConfig.java
+++ src/backend/spring/src/main/java/com/example/demo/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.example.demo.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+
                         "http://localhost:3000", // Allow requests from localhost:3000 (dev)
                         "http://frontend:3000", // Allow requests from frontend service
-                        "http://backend:8080"   // Allow requests from backend service
+                        "http://backend:8080",  // Allow requests from backend service
+                        "https://tourwise.org" // Allow requests from your production
+
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
