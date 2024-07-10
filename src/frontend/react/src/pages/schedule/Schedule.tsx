aee1ac8db224263c4796eee95e67aa6496c19f7f
Jorge Durán <67933217+HelloMum@users.noreply.github.com>
2024-07-10 11:55:36 +0100

    Update Schedule.tsx
    Please use the proper proxy to avoid problems with cors and tunneling

--- src/frontend/react/src/pages/schedule/Schedule.tsx
+++ src/frontend/react/src/pages/schedule/Schedule.tsx
@@ -56,7 +56,7 @@ const Schedule: React.FC = () => {
 
   const fetchBusynessData = async (date: string) => {
     try {
-      const response = await fetch(`http://localhost:8080/busyness/predict_by_date_range?startDate=${date}&endDate=${date}`, {
+      const response = await fetch(`/api/busyness/predict_by_date_range?startDate=${date}&endDate=${date}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
