74413b896ab7fc1b87cdaa0c3cea03f92cf0b5e5
lasa1015 <luosha1015@gmail.com>
2024-07-10 20:27:09 +0100

    add a badge to the list button

--- src/frontend/react/src/components/list/Btn_List.tsx
+++ src/frontend/react/src/components/list/Btn_List.tsx
@@ -1,40 +1,67 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React, { useContext } from 'react';
+import { Button, Box } from '@mui/material';
 import { FormatListBulletedRounded } from '@mui/icons-material';
+import { ListContext } from '../../contexts/ListContext';
 
 interface BtnListProps {
   onClick: () => void;
 }
 
 const Btn_List: React.FC<BtnListProps> = ({ onClick }) => {
+  const { listItems } = useContext(ListContext);
+
   return (
-    <Button
-      sx={{
-        position: 'absolute',
-        top: '100px',
-        right: '7px',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 10,
-        borderRadius: '20px',
-        cursor: 'pointer',
-        backgroundColor: '#ffc147',
-        width: '90px',
-        paddingX: '10px',
-        color: 'white',
-        '&:hover': {
+    <Box sx={{ 
+      position: 'absolute', display: 'inline-block', 
+      top: '100px', right: '130px' }}>
+      <Button
+        sx={{
+          position: 'absolute',
+          zIndex: 10,
+          borderRadius: '20px',
+          cursor: 'pointer',
+          backgroundColor: '#ffc147',
+          width: '90px',
+          paddingX: '10px',
+          color: 'white',
+          '&:hover': {
+            boxShadow: '1',  
+          },
+          '&:active': {
+            boxShadow: 'none',  
+          },
           boxShadow: '1',  
-        },
-        '&:active': {
-          boxShadow: 'none',  
-        },
-        boxShadow: '1',  
-      }}
-      variant="contained"
-      startIcon={<FormatListBulletedRounded />}  
-      onClick={onClick}
-    >
-      List
-    </Button>
+        }}
+        variant="contained"
+        startIcon={<FormatListBulletedRounded />}  
+        onClick={onClick}
+      >
+        List
+      </Button>
+      {listItems.length > 0 && (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '-10px',
+            right: '-95px',
+            backgroundColor: 'white',
+            borderRadius: '50%',
+            width: '23px',
+            height: '23px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: '#363636',
+            fontSize: '13px',
+            fontWeight: '500',
+            zIndex: 10,
+            boxShadow: '2px 2px 6px rgba(0, 0, 0, 0.2)', 
+          }}
+        >
+          {listItems.length}
+        </Box>
+      )}
+    </Box>
   );
 };
 
