995b3870b435adc07f46180465d4de231364cf46
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-07-14 20:18:11 +0300

    feat(user_management): add CustomUserDetailsService to give SecurityConfig what it needs about the user

---
+++ src/backend/spring/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+//import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.example.demo.model.User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    }
+}
