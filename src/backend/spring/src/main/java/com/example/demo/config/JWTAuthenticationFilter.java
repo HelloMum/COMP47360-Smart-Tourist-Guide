6adb3f6aa2d6c259aa65eff5aa3fff168df0847e
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-07-15 16:17:22 +0300

    fix(login): remove authentication filter for spring boot security
    - We will comment out these parts, later if we want to spend time and fix the login issue, we can simply uncomment it and continue working on it

--- src/backend/spring/src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
+++ src/backend/spring/src/main/java/com/example/demo/config/JWTAuthenticationFilter.java
@@ -1,45 +1,45 @@
-package com.example.demo.config;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Date;
-
-public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    private final AuthenticationManager authenticationManager;
-    private final String jwtSecret = "SecretKeyToGenJWTs";
-
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-    }
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
-        return authenticationManager.authenticate(authenticationToken);
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
-        String token = Jwts.builder()
-                .setSubject(authResult.getName())
-                .setExpiration(new Date(System.currentTimeMillis() + 864_000_000)) // 10 days
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                .compact();
-        response.addHeader("Authorization", "Bearer " + token);
-        response.getWriter().write("{\"token\": \"" + token + "\"}");
-    }
-}
+//package com.example.demo.config;
+//
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.Date;
+//
+//public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//
+//    private final AuthenticationManager authenticationManager;
+//    private final String jwtSecret = "SecretKeyToGenJWTs";
+//
+//    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+//        this.authenticationManager = authenticationManager;
+//    }
+//
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+//        String email = request.getParameter("email");
+//        String password = request.getParameter("password");
+//
+//        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
+//        return authenticationManager.authenticate(authenticationToken);
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+//        String token = Jwts.builder()
+//                .setSubject(authResult.getName())
+//                .setExpiration(new Date(System.currentTimeMillis() + 864_000_000)) // 10 days
+//                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+//                .compact();
+//        response.addHeader("Authorization", "Bearer " + token);
+//        response.getWriter().write("{\"token\": \"" + token + "\"}");
+//    }
+//}
