9da7b9edee648efbf6c05b56412475c58f0b373c
Skyeeee <boyu.wang01@outlook.com>
2024-07-18 23:42:14 +0100

    - Delete useless code

--- src/backend/spring/src/main/java/com/example/demo/service/PredictionScheduler.java
+++ src/backend/spring/src/main/java/com/example/demo/service/PredictionScheduler.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
