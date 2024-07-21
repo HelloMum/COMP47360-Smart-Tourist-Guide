c29e900990d4e4a47ba09a7b36465402acaaa11c
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-07-21 21:58:26 +0300

    feat(dashboard): Add findByUserId method to ItinerarySavedRepository

--- src/backend/spring/src/main/java/com/example/demo/repository/ItinerarySavedRepository.java
+++ src/backend/spring/src/main/java/com/example/demo/repository/ItinerarySavedRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.ItinerarySaved;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItinerarySavedRepository extends JpaRepository<ItinerarySaved, Long> {
+    List<ItinerarySaved> findByUserId(Long id);
 }
