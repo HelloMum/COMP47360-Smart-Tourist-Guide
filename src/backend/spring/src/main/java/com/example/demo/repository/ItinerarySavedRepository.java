4bbb271175db559fd9960972e752ec11f9858dc2
TugrulGit <business.tugrul.yilmaz@gmail.com>
2024-07-21 20:46:04 +0300

    feat(repository): Add ItinerarySavedItemsRepository and ItinerarySavedRepository
    This commit adds the `ItinerarySavedItemsRepository` interface to the project. The repository is responsible for handling CRUD operations for the `ItinerarySavedItems` entity. It extends the `JpaRepository` interface provided by Spring Data JPA.

---
+++ src/backend/spring/src/main/java/com/example/demo/repository/ItinerarySavedRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.ItinerarySaved;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItinerarySavedRepository extends JpaRepository<ItinerarySaved, Long> {
+}
