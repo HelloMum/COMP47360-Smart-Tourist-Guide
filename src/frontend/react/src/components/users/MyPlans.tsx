b751f0331f545918396d32eac4bab71eb2df31a1
Jorge Durán <67933217+HelloMum@users.noreply.github.com>
2024-07-28 00:47:56 +0100

    Frontend issue fetching save jorge (#148)
    * No cache on save
    
    * No cache on save
    
    * fix(lastUpdated): add context to update once a new itinerary saved on db
    
    - this will be used to trigger useEffect in MyPlans.tsx to fetch all itineraries including latest
    
    * fix(lastUpdated): add context to set lastUpdatedContext once a new itinerary saved on db
    
    - this will be used to trigger useEffect in MyPlans.tsx to fetch all itineraries including latest
    
    * fix(lastUpdated): add context to use lastUpdatedContext once a new itinerary saved on db
    
    - Fetches all itineraries again
    
    * fix(lastUpdated): wrap app with LastUpdatedContext
    
    ---------
    
    Co-authored-by: TugrulGit <business.tugrul.yilmaz@gmail.com>

--- src/frontend/react/src/components/users/MyPlans.tsx
+++ src/frontend/react/src/components/users/MyPlans.tsx
@@ -1,72 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Box, Typography } from '@mui/material';
 import ItineraryCard from './ItineraryCard';
+import { useLastUpdatedContext } from "../../contexts/LastUpdatedContext";
 
 const FetchItinerary: React.FC = () => {
-    const [itinerary, setItinerary] = useState<any[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
+  const [itinerary, setItinerary] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
+  const { lastUpdate } = useLastUpdatedContext(); // Use the context to access lastUpdate
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
 
         const fetchItinerary = async () => {
             try {
-                const response = await fetch(`/api/itinerary/user?token=${token}`);
+                const url = `/api/itinerary/user?token=${token}&timestamp=${new Date().getTime()}`;
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Cache-Control': 'no-cache, no-store, must-revalidate',
+                        'Pragma': 'no-cache',
+                        'Expires': '0'
+                    }
+                });
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
                 }
                 const data = await response.json();
                 console.log("saved itinerary data:", data); // Log the fetched data
 
-                // Convert the object with numerical keys to an array
-                const itineraryArray = Object.keys(data).map(key => data[key]);
-                setItinerary(itineraryArray);
-            } catch (error) {
-                setError(error.message);
-            } finally {
-                setLoading(false);
-            }
-        };
+        // Convert the object with numerical keys to an array
+        const itineraryArray = Object.keys(data).map((key) => data[key]);
+        setItinerary(itineraryArray);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchItinerary();
-    }, []);
+    fetchItinerary();
+  }, [lastUpdate]); // Add lastUpdate to the dependency array
 
-    if (loading) return <Box marginLeft={10}>Loading...</Box>;
-    if (error) return <Box>Error: {error}</Box>;
+  if (loading) return <Box marginLeft={10}>Loading...</Box>;
+  if (error) return <Box>Error: {error}</Box>;
 
-    return (
-        <Box
-            sx={{
-                overflowY: 'scroll',
-                height: '100%',
-                marginLeft:'70px',
-                msOverflowStyle: 'none',  /* IE and Edge */
-                scrollbarWidth: 'none',  /* Firefox */
-                '&::-webkit-scrollbar': {
-                    display: 'none',
-                },
-            }}
-        >
-           <h2 style={{ marginLeft: 0, marginBottom: '25px', fontFamily: '"Lexend", sans-serif', fontSize: '22px' }}>My Plans
-            </h2>
+  return (
+    <Box
+      sx={{
+        overflowY: "scroll",
+        height: "100%",
+        marginLeft: "70px",
+        msOverflowStyle: "none" /* IE and Edge */,
+        scrollbarWidth: "none" /* Firefox */,
+        "&::-webkit-scrollbar": {
+          display: "none",
+        },
+      }}
+    >
+      <h2
+        style={{
+          marginLeft: 0,
+          marginBottom: "25px",
+          fontFamily: '"Lexend", sans-serif',
+          fontSize: "22px",
+        }}
+      >
+        My Plans
+      </h2>
 
-            
-            {itinerary.length === 0 ? (
-                <Box>No Plan found</Box>
-            ) : (
-                itinerary.map((journey, index) => (
-                    <ItineraryCard
-                        key={index}
-                        itinerary={journey}
-                        index={index}
-                        onStartTimeClick={(startTime) => console.log(startTime)}
-                        highlightedStartTime={null}
-                    />
-                ))
-            )}
-        </Box>
-    );
+      {itinerary.length === 0 ? (
+        <Box>No Plan found</Box>
+      ) : (
+        itinerary.map((journey, index) => (
+          <ItineraryCard
+            key={index}
+            itinerary={journey}
+            index={index}
+            onStartTimeClick={(startTime) => console.log(startTime)}
+            highlightedStartTime={null}
+          />
+        ))
+      )}
+    </Box>
+  );
 };
 
 export default FetchItinerary;
